8892342aca99399985b8eb9435a328047a596b9b
Юля <potemkin.vg@yandex.ru>
2022-10-02 20:59:27 +0300

    Задание выполнено!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var WeightOfTheFirstFighter = 78.2;
+        var WeightOfTheSecondFighter = 82.7;
+        var TotalWeight = WeightOfTheFirstFighter + WeightOfTheSecondFighter;
+        System.out.println("Общий вес двух бойцов " + TotalWeight + " кг!");
+        var DifferenceWeight = WeightOfTheSecondFighter - WeightOfTheFirstFighter;
+        System.out.println("Второй боец весит больше первого на " + DifferenceWeight + " кг!");
+        var DifferenceWeightTwo = WeightOfTheSecondFighter % WeightOfTheFirstFighter;
+        System.out.println("Первый боец весит меньше второго на " + DifferenceWeightTwo + " кг!");
+
 
     }
 }
